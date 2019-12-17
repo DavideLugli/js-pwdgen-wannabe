07d4a59ee9a77ca86ff9ae5a4651f6e2b763a0c8
Davide Lugli <luglidavide.web@gmail.com>
2019-12-17 15:49:29 +0100

    Final Commit

--- js/myscript.js
+++ js/myscript.js
@@ -1,7 +1,12 @@
 // Chiedi all’utente il suo nome
-
+var userName = prompt('Come ti chiami?');
+console.log(userName);
+document.getElementById('nome-utente').innerHTML=userName;
 // poi chiedi il suo cognome
-
+var userSurname = prompt('Qual è il tuo cognome?');
+console.log(userSurname);
 // poi chiedi il suo colore preferito
-
+var userColor = prompt('Qual è il tuo colore preferito?');
+console.log(userColor);
 // Infine scrivi sulla pagina (html)
+document.getElementById('psw-container').innerHTML=userName + userSurname + userColor + '19';
